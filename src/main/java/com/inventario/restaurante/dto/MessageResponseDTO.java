96aec341ab089cd62c652761626d22dbb3dde3e0
krisgallego <krisgallego7@gmail.com>
2026-03-10 09:19:32 -0500

    Inventario restaurante finalizado

--- src/main/java/com/inventario/restaurante/dto/MessageResponseDTO.java
+++ src/main/java/com/inventario/restaurante/dto/MessageResponseDTO.java
@@ -1,5 +1,14 @@
 package com.inventario.restaurante.dto;
 
 public class MessageResponseDTO {
-    
+
+    private String message;
+
+    public MessageResponseDTO(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
 }
